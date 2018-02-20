a6d4a8284c5acd5e09117e98b0af0ebd70dc667b
yinpeng <01198313@yto.net.cn>
2018-02-20 10:34:58 +0800

    整合Rocketmq

---
+++ test-rocketmq/src/main/java/com/yp/testrocketmq/TestRocketmqApplication.java
@@ -0,0 +1,12 @@
+package com.yp.testrocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRocketmqApplication.class, args);
+	}
+}
